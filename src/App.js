39f61c8f7e547558aac66873d6e835468fd2b51c
Alexander Kuchuk <a.kuchuk@uptrader.io>
2020-10-25 17:18:09 +0300

    added structure plan

--- src/App.js
+++ src/App.js
@@ -3,6 +3,18 @@ import './App.css';
 const App = () => {
   return (
     <div className="app">
+      {/* {Header} */}
+
+      {/* {Title + Select input dropdown field} */}
+
+      {/* {InfoBoxs} */}
+      {/* {InfoBoxs} */}
+      {/* {InfoBoxs} */}
+
+      {/* {Table} */}
+      {/* {Graph} */}
+
+      {/* {Map} */}
     </div>
   );
 }
