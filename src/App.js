0ad8bf8e51d06a41ffcfb31447f8bb59650ff0cd
Alexander Kuchuk <a.kuchuk@uptrader.io>
2020-10-25 20:50:31 +0300

    fix Map and InfoBox

--- src/App.js
+++ src/App.js
@@ -1,30 +1,34 @@
 import React, { useState, useEffect } from 'react'
+import numeral from "numeral";
 import { Card, CardContent, FormControl, MenuItem, Select } from '@material-ui/core';
 
 import { InfoBox, Map, Table, LineGraph } from './components';
 import { API_ALL, API_COUNTRIES, getData, getCountriesData, fetchData } from './api';
 
+import { sortData, prettyPrintStat } from './utils';
 import './App.css';
-import { sortData } from './utils';
+import "leaflet/dist/leaflet.css";
 
 const App = () => {
   const [countries, setCountries] = useState([]);
   const [country, setInputCountry] = useState("worldwide");
   const [countryInfo, setCountryInfo] = useState({});
   const [tableData, setTableData] = useState([]);
-  // const [mapCountries, setMapCountries] = useState([]);
+  const [mapCountries, setMapCountries] = useState([]);
   const [casesType, setCasesType] = useState("cases");
-  // const [mapCenter, setMapCenter] = useState({ lat: 34.80746, lng: -40.4796 });
-  // const [mapZoom, setMapZoom] = useState(3);
+  const [mapCenter, setMapCenter] = useState({ lat: 59.919996, lng: 30.318382 });
+  const [mapZoom, setMapZoom] = useState(2);
 
   useEffect(() => {
     getData().then(data => setCountryInfo(data))
+
   }, [setCountryInfo]);
 
   useEffect(() => {
     getCountriesData().then(({data, countries}) => {
       const sortedData = sortData(data)
       setTableData(sortedData)
+      setMapCountries(data);
       setCountries(countries)
     })
   }, [setCountries]);
@@ -37,7 +41,12 @@ const App = () => {
         ? API_ALL
         : `${API_COUNTRIES}/${countryCode}`;
 
-      fetchData(url).then(data => setCountryInfo(data))
+      fetchData(url).then(data => {
+        setCountryInfo(data);
+        setInputCountry(countryCode);
+        setMapCenter([data.countryInfo.lat, data.countryInfo.long]);
+        setMapZoom(6);
+      })
 
       setInputCountry(countryCode);
   }
@@ -63,27 +72,31 @@ const App = () => {
 
         <div className="app__stats">
           <InfoBox
-            onClick={() => setCasesType("cases")}
-            title="Coronavirus cases"
-            total={countryInfo.cases}
-            cases={countryInfo.todayCases}
-          />
-          <InfoBox
-            onClick={() => setCasesType("recovered")}
-            title="Recovered"
-            total={countryInfo.recovered}
-            cases={countryInfo.todayRecovered}
-          />
-          <InfoBox
-            onClick={() => setCasesType("deaths")}
-            title="Deaths"
-            total={countryInfo.deaths}
-            cases={countryInfo.todayDeaths}
+            onClick={(e) => setCasesType("cases")}
+            title="Coronavirus Cases"
             isRed
+            active={casesType === "cases"}
+            cases={prettyPrintStat(countryInfo.todayCases)}
+            total={numeral(countryInfo.cases).format("0.0a")}
+          />
+          <InfoBox
+            onClick={(e) => setCasesType("recovered")}
+            title="Recovered"
+            active={casesType === "recovered"}
+            cases={prettyPrintStat(countryInfo.todayRecovered)}
+            total={numeral(countryInfo.recovered).format("0.0a")}
+          />
+          <InfoBox
+            onClick={(e) => setCasesType("deaths")}
+            title="Deaths"
+            isRed
+            active={casesType === "deaths"}
+            cases={prettyPrintStat(countryInfo.todayDeaths)}
+            total={numeral(countryInfo.deaths).format("0.0a")}
           />
         </div>
 
-        <Map />
+        <Map zoom={mapZoom} center={mapCenter} countries={mapCountries} casesType={casesType} />
       </div>
       <Card className="app__right">
         <CardContent>
--- src/components/Map/Map.js
+++ src/components/Map/Map.js
@@ -1,25 +1,30 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-// import { Map as LeafletMap, TileLayer } from "react-leaflet";
+import { Map as LeafletMap, TileLayer } from "react-leaflet";
+
+import { showDataOnMap } from "../../utils";
 
 import './style.css'
 
 const Map = ({ countries, casesType, center, zoom }) => {
     return (
       <div className="Map">
-        {/* <LeafletMap center={center} zoom={zoom}>
+        <LeafletMap center={center} zoom={zoom}>
           <TileLayer
-            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
             attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+            url='https://{s}.tile.osm.org/{z}/{x}/{y}.png'
           />
           {showDataOnMap(countries, casesType)}
-        </LeafletMap> */}
+        </LeafletMap>
       </div>
     )
 }
 
 Map.propTypes = {
-
+  countries: PropTypes.array.isRequired,
+  casesType: PropTypes.string.isRequired,
+  center: PropTypes.oneOfType([PropTypes.array,PropTypes.object]).isRequired,
+  zoom: PropTypes.number.isRequired,
 }
 
 export default Map
